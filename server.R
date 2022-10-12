5e49b763401526678672e6550c72088a1d012a48
Sanghee Kang <dr.kasaha@gmail.com>
2022-10-12 17:33:48 +0900

    Major update
    Major update before the submission of paper.

--- server.R
+++ server.R
@@ -1,9 +1,12 @@
 
+
 library(tidyverse)
 library(data.table)
 library(shiny)
 library(plotly)
 library(heatmaply)
+library(shinycssloaders)
+library(HGNChelper)
 
 library(BiocManager)
 options(repos = BiocManager::repositories())
@@ -13,35 +16,37 @@ library(impute)
 source("functions.R")
 
 trainingGeneMatrix <-
-    fread("./www/trainingMatrix.txt") %>% as.data.frame()
+  fread("./www/trainingMatrix.txt") %>% as.data.frame()
 trainingClass <-
-    fread("./www/trainingClass.txt") %>% as.data.frame()
+  fread("./www/trainingClass.txt") %>% as.data.frame()
 trainingGenenames <-
-    fread("./www/geneNames.txt") %>% as.data.frame()
+  fread("./www/geneNames.txt") %>% as.data.frame()
 
 trainingX <- trainingGeneMatrix %>% data.matrix() %>% impute.knn()
 trainingX <- trainingX$data
 
-Genenames <- trainingGenenames %>% t %>% as.vector()
+Genenames_precheck <-
+  trainingGenenames %>% t %>% as.vector() %>% checkGeneSymbols
+Genenames <- Genenames_precheck[3] %>% t %>% as.vector()
 
 ## transform 'class name' to numeric vector
 levOfClass <- trainingClass %>% t %>% as.factor() %>% levels()
 levOfClass_numeric <- levOfClass %>% as.factor() %>% as.numeric()
 table.levels <-
-    cbind(levOfClass, levOfClass_numeric) %>% as.data.frame()
+  cbind(levOfClass, levOfClass_numeric) %>% as.data.frame()
 trainingClass.m <-
-    left_join(trainingClass, table.levels, by = c("class" = "levOfClass"))
+  left_join(trainingClass, table.levels, by = c("class" = "levOfClass"))
 trainingY <-
-    trainingClass.m$levOfClass_numeric %>% t %>% as.character() %>% as.numeric()
+  trainingClass.m$levOfClass_numeric %>% t %>% as.character() %>% as.numeric()
 
 ## merging dataset for training
 mydata <-
-    list(
-        x = trainingX,
-        y = trainingY,
-        genenames = Genenames,
-        geneid = c(1:length(Genenames))
-    )
+  list(
+    x = trainingX,
+    y = trainingY,
+    genenames = Genenames,
+    geneid = c(1:length(Genenames))
+  )
 
 ## analysis start : Training
 model <- pamr.train(mydata)
@@ -50,148 +55,233 @@ model <- pamr.train(mydata)
 
 model.cv <- pamr.cv(fit = model, data = mydata)
 
-## Threshold 0 by analysis
+## Threshold 1.628 by analysis
 Delta <- 0
 
 ## analysis start : centroid
 centroid_gene <-
-    pamr.listgenes(model, mydata, Delta, genenames = TRUE) %>% as.data.frame
+  pamr.listgenes(model, mydata, Delta, genenames = TRUE) %>% as.data.frame
 centroid_gene$id <- centroid_gene$id %>% as.numeric()
 centroid_gene <- centroid_gene %>% arrange(id)
 colnames(centroid_gene)[c(3:(2 + length(levOfClass)))] <- levOfClass
 
 # Modeling finish
 
+# server functions----
 shinyServer(function(input, output) {
-    # reactive function
-    geneExprDataIn <- reactive({
-        inFile <- input$geneExprfile
-        req(inFile)
-        f <-
-            fread(inFile$datapath) %>% as.data.frame() %>% kasa.duplicationRemovalBySD()
-        
-        data.raw <- f
-        colnames(data.raw)[1] <- c("genenames")
-        testDataset.modi <-
-            left_join(x = trainingGenenames,
-                      y = data.raw,
-                      by = c("genenames"))
-       
-        # status <<- "File upload complete. The next step for analysis is ready."
-        # output$status <- renderText(status)
-        return(testDataset.modi)
-    })
+  preparationText <- "Upload your dataset into the box on the left."
+  
+  # reactive function ----
+  
+  
+  
+  
+  geneExprDataIn <- reactive({
+    inFile <- input$geneExprfile
+    req(inFile)
+    f <-
+      fread(inFile$datapath) %>% as.data.frame() %>% kasa.duplicationRemovalBySD()
     
-    reactiveDataStandardization <- reactive({
-        if (input$standardizationType == "medianCenter") {
-            data.raw <- geneExprDataIn() %>% kasa.geneMedianCentering()
-        } else {
-            
-            data.raw <-
-                geneExprDataIn() %>% kasa.geneMedianCentering() %>% kasa.geneStandardization()
-           
-        }
-        testX.p <- data.raw[-1] %>% data.matrix() %>% impute.knn()
-        testX <- testX.p$data
-        
-        return(testX)
-    })
+    data.raw <- f
     
-    reavticResultSummaryPlot <- reactive({
-        dataPlotly <- DoPIC100prediction()
-        fig <- plot_ly(dataPlotly,y = ~posterior, x=~Class, color=~Class, text=~paste(Sample), type = 'box', jitter=0.3,boxpoints = 'all')
-        return(fig)
-        
-    })
-    reavticResultPiePlot <- reactive({
-        dataPlotly <- DoPIC100prediction()
-        fig <- plot_ly(dataPlotly, labels = ~Class, type = 'pie')
-        return(fig)
-        
-    })
-    # event reactive
-    DoPIC100prediction <- eventReactive(input$doPrediction, {
-        output$preparation <- renderText("")
-        output$preparation2 <- renderText("")
-        
-        testX <- reactiveDataStandardization()
-        print("prediction start")
-        
-        res.class <-
-            pamr.predict(
-                fit = model,
-                newx = testX,
-                threshold = Delta,
-                type = "class"
-            ) %>% as.character() %>% as.numeric()
-        res.class.t <- levOfClass[res.class]
-        
-        res.probability <-
-            pamr.predict(
-                fit = model,
-                newx = testX,
-                threshold = Delta,
-                type = "posterior"
-            )
-        res.probability.m <- res.probability %>% round(digits = 3)
-        res.probability.t <-
-            apply(res.probability, 1, max) %>% round(digits = 3)
-        
-        res.ID <- colnames(testX)
-        
-        res.table <-
-            cbind(res.ID,
-                  res.class.t,
-                  res.probability.t,
-                  res.probability.m) %>% as.data.frame()
-        colnames(res.table) <-
-            c(
-                "Sample",
-                "Class",
-                "posterior",
-                "post.A",
-                "post.B",
-                "post.C",
-                "post.D",
-                "post.E",
-                "post.F"
-            )
-        return(res.table)
-    })
-    reavticResultHeatmapPlot <- reactive({
-        dataPlotly <- DoPIC100prediction()
-        dataRaw <- reactiveDataStandardization()
-        
-        data.sample.order <- dataPlotly %>% arrange(Class,posterior) 
-        data.sample.order.vector <- data.sample.order[,1] %>% as.character()
-        data.sample.order.labels <- data.sample.order %>% mutate(labels=paste(Class,":",Sample))
-        data.sample.order.labels <- data.sample.order.labels$labels %>% as.vector()
-        
-        data.heatmap <- dataRaw 
-        
-        data.heatmap <- data.heatmap[rev(1:nrow(data.heatmap)),data.sample.order.vector]
-        
-        
-        fig <- plot_ly(z = data.heatmap, type = "heatmap",colors = colorRamp(c("#009900","#00FF00","black","#FF0000","#990000")),y=rev(Genenames),x=data.sample.order.labels, zauto = FALSE, zmin = -4, zmax = 4) %>% layout(title="The Heatmap of your dataset",font=list(family="Arial"),xaxis=list(tickangle=45))
-        
-        return(fig)
-    })
-    # output
-    output$downloadResults <- downloadHandler(
-        filename = function() {
-            "Result.txt"
-        },
-        content = function(file) {
-            contents.table <- DoPIC100prediction()
-            write_delim(contents.table, file, delim = "\t",na = "")
-        },
-        contentType = "text/plain"
-    )
-    # ouput rendering
-    output$tablesTemp <- renderTable(DoPIC100prediction())
-    output$resultSummaryPlot <- renderPlotly(reavticResultSummaryPlot())
-    output$resultPiePlot <- renderPlotly(reavticResultPiePlot())
-    output$resultHeatmapPlot <- renderPlotly(reavticResultHeatmapPlot())
-    output$preparation <- renderText("Insert Your Dataset")
-    output$preparation2 <- renderText("Insert Your Dataset")
+    HGNCcheck <- data.raw[1] %>% t() %>% as.vector()
+    checkedSymbols <- checkGeneSymbols(HGNCcheck)
+    data.raw[1] <- checkedSymbols[3]
+    HGNCcheckdata_FALSE <-
+      checkedSymbols %>% filter(Approved == "FALSE")
+    HGNCcheckdata_FALSE <- HGNCcheckdata_FALSE[c(1, 3)]
+    colnames(HGNCcheckdata_FALSE) <-
+      c("YourGene.Symbols", "HGNCsymbols.Converted")
+    # print(HGNCcheckdata_FALSE)
+    output$tablesConvertedGeneSymbols <-
+      renderTable(HGNCcheckdata_FALSE)
+    colnames(data.raw)[1] <- c("genenames")
+    
+    
+    testDataset.modi <-
+      left_join(x = trainingGenenames,
+                y = data.raw,
+                by = c("genenames"))
+    MissingValuseGenes <-
+      testDataset.modi[!complete.cases(testDataset.modi), ]
+    colnames(MissingValuseGenes)[1] <-
+      c("Your symbols including Missing values")
+    output$MissingValuesSymbols <-
+      renderTable(MissingValuseGenes[1])
+    
+    
+    return(testDataset.modi)
+  })
+  
+  reactiveDataStandardization <- reactive({
+    
+    # standardization
+    if (input$standardizationType == "NoneS") {
+      data.raw <- geneExprDataIn() 
+      # } else if (input$standardizationType == "medianCenter") {
+      #     data.raw <- geneExprDataIn() %>% kasa.geneMedianCentering()
+    } else if (input$standardizationType == "devidedBySD")
+    {
+      data.raw <-
+        geneExprDataIn() %>% kasa.geneMedianCentering() %>% kasa.geneStandardization()
+      
+    }
+    
+    # impute
+    if (ncol(data.raw) < 3) {
+      rowmax.value <- 1
+    } else {
+      rowmax.value <- 0.5
+    }  # rowmax decision by the number of samples
+    
+    testX.p <- data.raw[-1] %>% data.matrix() %>% impute.knn(rowmax = rowmax.value)
+    testX <- testX.p$data
+    
+    return(testX)
+  })
+  
+  reavticResultSummaryPlot <- reactive({
+    dataPlotly <- DoPIC100prediction()
+    fig <-
+      plot_ly(
+        dataPlotly,
+        y = ~ posterior,
+        x =  ~ Class,
+        color =  ~ Class,
+        text =  ~ paste(Sample),
+        type = 'box',
+        jitter = 0.3,
+        boxpoints = 'all'
+      )
+    return(fig)
+    
+  })
+  reavticResultPiePlot <- reactive({
+    dataPlotly <- DoPIC100prediction()
+    fig <- plot_ly(dataPlotly, labels = ~ Class, type = 'pie')
+    return(fig)
+    
+  })
+  reavticResultHeatmapPlot <- reactive({
+    dataPlotly <- DoPIC100prediction()
+    dataRaw <- reactiveDataStandardization()
+    
+    data.sample.order <-
+      dataPlotly %>% arrange(Class, posterior)
+    data.sample.order.vector <-
+      data.sample.order[, 1] %>% as.character()
+    data.sample.order.labels <-
+      data.sample.order %>% mutate(labels = paste(Class, ":", Sample))
+    data.sample.order.labels <-
+      data.sample.order.labels$labels %>% as.vector()
+    
+    data.heatmap <- dataRaw
+    
+    data.heatmap <-
+      data.heatmap[rev(1:nrow(data.heatmap)), data.sample.order.vector]
+    
+    
+    fig <-
+      plot_ly(
+        z = data.heatmap,
+        type = "heatmap",
+        colors = colorRamp(c(
+          "#009900", "#00FF00", "black", "#FF0000", "#990000"
+        )),
+        y = rev(Genenames),
+        x = data.sample.order.labels,
+        zauto = FALSE,
+        zmin = -4,
+        zmax = 4
+      ) %>% layout(
+        title = "The Heatmap of your dataset",
+        font = list(family = "Arial"),
+        xaxis = list(tickangle = 45)
+      )
+    
+    return(fig)
+  })
+  # event reactive ----
+  DoPIC100prediction <- eventReactive(input$doPrediction, {
+    testX <- reactiveDataStandardization()
+    
+    preparationText <<- ""
+    output$preparation <- renderText(preparationText)
+    output$preparation2 <- renderText(preparationText)
+    
+    print("prediction start")
+    
+    res.class <-
+      pamr.predict(
+        fit = model,
+        newx = testX,
+        threshold = Delta,
+        type = "class"
+      ) %>% as.character() %>% as.numeric()
+    res.class.t <- levOfClass[res.class]
+    
+    res.probability <-
+      pamr.predict(
+        fit = model,
+        newx = testX,
+        threshold = Delta,
+        type = "posterior"
+      )
+    res.probability.m <- res.probability %>% round(digits = 3)
+    res.probability.t <-
+      apply(res.probability, 1, max) %>% round(digits = 3)
+    
+    res.ID <- colnames(testX)
+    
+    res.table <-
+      cbind(res.ID,
+            res.class.t,
+            res.probability.t,
+            res.probability.m) %>% as.data.frame()
+    colnames(res.table) <-
+      c(
+        "Sample",
+        "Class",
+        "posterior",
+        "post.CGS1",
+        "post.CGS2",
+        "post.CGS3",
+        "post.CGS4",
+        "post.CGS5",
+        "post.CGS6"
+      )
+    return(res.table)
+  })
+  
+  # output
+  output$downloadResults <- downloadHandler(
+    filename = function() {
+      "Result.txt"
+    },
+    content = function(file) {
+      contents.table <- DoPIC100prediction()
+      write_delim(contents.table, file, delim = "\t", na = "")
+    },
+    contentType = "text/plain"
+  )
+  output$downloadExample <- downloadHandler(
+    filename = function() {
+      "testMatrix.txt"
+    },
+    content = function(file) {
+      contents.table <- fread("./www/testDataset/testMatrix.txt")
+      write_delim(contents.table, file, delim = "\t", na = "")
+    },
+    contentType = "text/plain"
+  )
+  # ouput rendering
+  output$tablesTemp <- renderTable(DoPIC100prediction())
+  output$resultSummaryPlot <-
+    renderPlotly(reavticResultSummaryPlot())
+  output$resultPiePlot <- renderPlotly(reavticResultPiePlot())
+  output$resultHeatmapPlot <-
+    renderPlotly(reavticResultHeatmapPlot())
+  output$preparation <- renderText(preparationText)
+  output$preparation2 <- renderText(preparationText)
+  
 })
--- ui.R
+++ ui.R
@@ -10,69 +10,115 @@
 library(shiny)
 library(plotly)
 library(heatmaply)
+library(shinycssloaders)
+library(HGNChelper)
+library(shinythemes)
 
 library(BiocManager)
 options(repos = BiocManager::repositories())
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    # Application title
-    titlePanel("GPICS120 prediction by Ju-Seog's Lab"),
+  theme = shinytheme("journal"),
+
+  titlePanel("GPICS 120 classfication for Gastric cancer"),
+ 
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      tags$img(
+        src = "Logo_GPICS120.png",
+        width = 230,
+        height = 60
+      ),
+      br(),
+      br(),
+      fileInput(
+        'geneExprfile',
+        h4('Choose or Drag&drop the mRNA expression file(txt/csv)'),
+        accept = c('text/csv',
+                   'text/comma-separated-values,text/plain',
+                   '.csv')
+      ),
+      radioButtons(
+        'standardizationType',
+        'Standardization',
+        c(
+          'Non-standardization (For single-sample use)' = 'NoneS',
+          # 'Mendian-centering only' = 'medianCenter',
+          'Median-centering and dividing by SD' = 'devidedBySD'
+        ),
+        'devidedBySD'
+      ),
+      actionButton("doPrediction", "Prediction", class = "btn-primary"),
+      br(),
+      br(),
+      downloadButton('downloadResults', 'Download result table')
+    ),
     
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            fileInput(
-                'geneExprfile',
-                h4('Choose Gene expression file(txt/csv)'),
-                accept = c('text/csv',
-                           'text/comma-separated-values,text/plain',
-                           '.csv')
-            ),
-            radioButtons(
-                'standardizationType',
-                'Standardization',
-                c(
-                    'Mendian-centering only' = 'medianCenter',
-                    'Median-centering and dividing by SD' = 'devidedBySD'
-                ),
-                'medianCenter'
-            ),
-            actionButton("doPrediction", "Prediction", class = "btn-primary"),
-            br(),
-            br(),
-            downloadButton('downloadResults', 'Download result table')
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          "Your dataset summary",
+          h3(textOutput("preparation")),
+          tableOutput("tablesConvertedGeneSymbols"),
+          tableOutput("MissingValuesSymbols"),
+          plotlyOutput("resultPiePlot") %>% withSpinner(color =
+                                                          "#0dc5c1"),
+          plotlyOutput("resultSummaryPlot")
         ),
         
-        # Show a plot of the generated distribution
-        mainPanel(tabsetPanel(
-            type = "tabs",
-            tabPanel("How to use",
-                     HTML("&nbsp; <p>Hi. This is the prediction tool for the analysis of Gastric cancer subtype using mRNA expression data.</p><p>Just upload your dataset. And press the prediction button. That's all."),
-                     HTML("</p><p>&nbsp;</p><p><b>Step 1. Prepare of dataset.</b></p>"
-                     ),
-                     img(
-                         src = "testDatasetExample.png",
-                         width = 500,
-                         height = 150
-                     ),
-                     HTML(
-                         "<p> &nbsp;</p><p> The first line contains the labels Name(<em>HUGO Gene Nomenclature</em>) followed by the identifiers for each sample in the dataset.The dataset is the gene-level transcription estimates, as in log2(x+1) transformed normalized count. &nbsp;</p><p>&nbsp;</p><p><b>Step 2. Standardization. </b> &nbsp;</p><p> Select the data standardization method. &nbsp;</p><p><b>Step 3. Prediction.</b> &nbsp;</p><p> Press the predictiop. &nbsp;</p><p><b>Step 4. Check out the results.</b> &nbsp;</p><p>After analysis, You can find the results at the result tab. The results of dataset could be downloaded using the download button.</p>"
-                     )
-                     
-            ),
-            tabPanel("Analysis Summary",
-                     h3(textOutput("preparation")),
-                     plotlyOutput("resultPiePlot"),
-                     plotlyOutput("resultSummaryPlot")
-            ),
-            tabPanel("Analysis Results",
-                     h3(textOutput("preparation2")),
-                     br(),br(),
-                     plotlyOutput("resultHeatmapPlot"),
-                     tableOutput("tablesTemp"),
-            )
-            
-        ))
+        tabPanel(
+          "Your results",
+          h4("GPICS120 classification"),
+          img(
+            src = "Fig1.png",
+            width = 350,
+            height = 300
+          ),
+          br(),
+          br(),
+          h3(textOutput("preparation2")),
+          plotlyOutput("resultHeatmapPlot") %>% withSpinner(color =
+                                                              "#0dc5c1"),
+          tableOutput("tablesTemp"),
+        ),
+        tabPanel(
+          "How to use",
+          HTML(
+            "&nbsp; <p>Hi. This is the prediction tool for the analysis of gastric cancer subtype using mRNA expression data.</p><p>Just upload your dataset. And press the prediction button. That's all. You can download example dataset from"
+          ),
+          tags$a(href = "https://raw.githubusercontent.com/kasaha1/GPICS120/main/www/testDataset/testMatrix.txt", " here"),
+          HTML("or the below button."),
+          br(),
+          downloadButton('downloadExample', 'Download Example dataset'),
+          br(),
+          HTML("</p><p>&nbsp;</p><p><b>Step 1. Prepare of dataset.</b></p>"),
+          img(
+            src = "testDatasetExample.png",
+            width = 500,
+            height = 150
+          ),
+          HTML(
+            "<p> &nbsp;</p><p> The first line contains the labels Name(<em>HUGO Gene Nomenclature</em>) followed by the identifiers for each sample in the dataset.The dataset is the gene-level transcription estimates, as in log2(x+1) transformed normalized count.&nbsp; </br>* The alias symbols are automatically converted to HGNC approved symbols by the HGNChelper package.&nbsp;</p><p>&nbsp;</p><p><b>Step 2. Standardization. </b> &nbsp;</p><p> Select the data standardization method. &nbsp;</p><p><b>Step 3. Prediction.</b> &nbsp;</p><p> Press the prediction button. &nbsp;</p><p><b>Step 4. Check out the results.</b> &nbsp;</p><p>After analysis, You can find the results at the result tab. The results of dataset could be downloaded using the download button.</p>"
+          )
+          
+        ),
+        tabPanel(
+          "About GPICS120",
+          HTML(
+            "<p>Gastric cancer (GC) is heterogeneous lethal disease in genomic and clinical level. By integrating 8 previously established genomic signatures for GC subtypes,<b> we identified 6 clinically and molecularly distinct genomic consensus subtypes (CGS)</b>. CGS1 is characterized by poorest prognosis, very high stem cell characteristics, and high IGF1 expression, but low genomic alterations.  CGS2 showed canonical epithelial gene expression patterns. CGS3 and CGS4 are characterized by high copy number alterations and low immune activity. However, CGS3 and CGS4 are different in high HER2 activation (CGS3) and SALL4 and KRAS activation (CSG4). CGS5 has highest mutation burden and moderately high immune activity that is characteristics of MSI-high tumors. Most of CGS6 tumors are EBV-positive and shows extremely high methylation and high immune activity.</p> "
+          ),
+          img(
+            src = "Fig2.png",
+            width = 500,
+            height = 600
+          )
+        )
+      )
     )
+  )
 ))
